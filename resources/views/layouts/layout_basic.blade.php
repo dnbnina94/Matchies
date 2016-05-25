64d78f51fc947a939723fee9046fafd802af9847
Milena <Milena>
2016-05-25 20:24:56 +0200

    Layout_basic added

---
+++ resources/views/layouts/layout_basic.blade.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html class="full" lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="author" content="Nina Grujic, Milena Filipovic, Branislava Ivkovic">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+	<link rel="shortcut icon" type="image/x-icon" href="images/puzle.ico" />
+
+    <title> Matchies - @yield('title') </title>
+
+    <link rel="shortcut icon" type="image/x-icon" href="images/heart.png" />
+    <!-- Bootstrap -->
+    <link href="bootstrap-3.3.6-dist/css/bootstrap.min.css" rel="stylesheet">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="bootstrap-3.3.6-dist/js/bootstrap.min.js"></script>
+
+	<link href="bootstrap-3.3.6-dist/css/proba.css" rel="stylesheet">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+
+
+  @yield('csslinks')
+  @yield('javascriptlinks')
+
+  @section('sewingBut')
+  <script>
+    $(document).ready(function(){
+        $("#sewingBut").click(function(){
+            $("#footerMenuNavBar").slideToggle("slow");
+        });
+    });
+  </script>
+     @show
+
+
+	</head>
+
+
+<body @yield('onloadfunction')>
+
+@yield('specialMessage')
+
+@section('settingsBoxContainer')
+<div id="settingsBoxContainer">
+	<div id="settingsBoxRowContainer">
+		<div id="settingsBoxCellContainer">
+			<div class="row">
+				<div class="col-md-5"></div>
+				<div class="settingsBox col-md-2" style="margin-left: 25px; margin-right: 25px">
+					<table width="100%" style="color: white; margin-bottom: 10px">
+						<tr>
+							<td style="font-weight: bold; font-size: 16px;">
+								Settings:
+							</td>
+							<td align="right" style="font-size: 16px; color: white">
+								<span class="glyphicon glyphicon-remove" onclick="hideSettings()" style="cursor: pointer"></span>
+							</td>
+						</tr>
+					</table>
+					<div style="padding-bottom: 10px; padding-top: 10px; border-bottom: 1px solid #B9BAB8"><a href="#" style="color: #AE0000">View your profile</a></div>
+					<div style="padding-bottom: 10px; padding-top: 10px; border-bottom: 1px solid #B9BAB8"><a href="/edit_profile" style="color: #AE0000">Edit your profile</a></div>
+					<div style="padding-bottom: 5px; padding-top: 10px;"><a href="index.html" style="color: #AE0000">Log out</a></div>
+				</div>
+				<div class="col-md-5"></div>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+@show
+
+
+
+
+@section('navbar')
+<nav class="navbar navbar-default navbar-custom" role="navigation" style="background: #AE0000; border: none">
+  <div class="container">
+
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="/home">
+        <img alt="Matchies" src="images/matchiespngwhite.png" width="100px">
+      </a>
+    </div>
+
+
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav navbar-right">
+            <li align="center"><a><span class="glyphicon glyphicon-cog " id="settings" style=" color:white; height: 22px; font-size: 16px; cursor: pointer" onclick="showSettings()"></span></a></li>
+            <li align="center"><a href="/messages"><span class="glyphicon glyphicon-comment " id="messages" style=" color:white; height: 22px; font-size: 16px"></span></a></li>
+            <li align="center"><a href="/notifications"><span class="glyphicon glyphicon-bell " id="alerts" style=" color:white; height: 22px; font-size: 16px"></span></a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+
+@show
+
+@yield('content')
+
+
+<!-- /footer -->
+@yield('footer')
+
+  </body>
+</html>
