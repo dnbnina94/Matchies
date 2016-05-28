10977f2d350fd466f85725acddcd9139f6f6d58c
Milena <mfilipovic94@gmail.com>
2016-05-28 18:14:03 +0200

    Admin and Moderator middleware dodato

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Carbon\Carbon;
+use App\Http\Requests;
+use App\User as User;
+use App\Registered_user as Registered_user;
+use App\Photo as Photo;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Storage as Storage;
+use Illuminate\Support\Facades\Hash as Hash;
+use Auth;
+
+
+class AdminController extends Controller
+{
+
+  public function ($value='')
+  {
+    # code...
+  }
+
+
+}
+
+?>
--- resources/views/auth/emails/password.blade.php
+++
@@ -1,2 +0,0 @@
-
-Click here to reset your password: {{ url('password/reset/'.$token) }}
