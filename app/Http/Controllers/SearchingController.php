70baedca665286f5e27a4c2359b0afe6ee1d03ac
Milena <mfilipovic94@gmail.com>
2016-05-29 04:17:36 +0200

    Search sada ukljucuje godine, dodato i za like i dislike, samo nije jos uradjeno da se pozovu na klik

--- app/Http/Controllers/SearchingController.php
+++ app/Http/Controllers/SearchingController.php
@@ -26,9 +26,9 @@ class SearchingController extends Controller
           $ageMax= $request->input('ageMax');
 
           //////////////////////////////////
-            $regUsers = Registered_user::all();
-            $number = $regUsers->count();
-            $iteration = -1;
+          $regUsers = Registered_user::all();
+          $number = $regUsers->count();
+          $iteration = -1;
 
 /////////////////////
                       if($gender=='Men and Women'){
@@ -42,82 +42,83 @@ class SearchingController extends Controller
                           $reg->interested_in= 'ff';
                         }
 
+                        $reg->minimal_age= $ageMin;
+                        $reg->max_age= $ageMax;
+
                         $reg->save();
 
 
         //    $nextUser= $regUsers ->first();
-        $returnUser= $regUsers ->first();
+        $returnUser= null;
       foreach ($regUsers as $nextUser) {
             $iteration ++ ;
-            $returnUser=$nextUser;
 
-            if($nextUser->id != $reg->id){
+            $dt = Carbon::now();
+            $years = $dt->diffInDays($nextUser->birth_date);
+            $years = floor($years/365);
 
-              if($reg->sex== 'f'){
+                    if($nextUser->id != $reg->id){
 
-                if($nextUser->interested_in == 'ff' or $nextUser->interested_in == 'fm' ){
+                      if( $years <= $ageMax and $years >= $ageMin){
 
-                              if($reg->interested_in == 'fm'){
-                                    break;
-                              }
+                        if($reg->sex== 'f'){
 
-                              if($reg->interested_in == 'mm'){
-                                  if($nextUser->sex == 'm'){
-                                    break;
-                                  }
-                              }
+                          if($nextUser->interested_in == 'ff' or $nextUser->interested_in == 'fm' ){
 
-                              if($reg->interested_in == 'ff'){
-                                if($nextUser->sex == 'f'){
-                                  break;
+                                        if($reg->interested_in == 'fm'){
+                                          $returnUser=$nextUser;
+                                              break;
+                                        }
+
+                                        if($reg->interested_in == 'mm'){
+                                            if($nextUser->sex == 'm'){
+                                              $returnUser=$nextUser;
+                                              break;
+                                            }
+                                        }
+
+                                        if($reg->interested_in == 'ff'){
+                                          if($nextUser->sex == 'f'){
+                                            $returnUser=$nextUser;
+                                            break;
+                                          }
+                                        }
+
+
+                                }
+
+                        }
+                        /////
+                        if($reg->sex== 'm'){
+                          if($nextUser->interested_in == 'mm' or $nextUser->interested_in == 'fm' ){
+
+                                      if($reg->interested_in == 'fm'){
+                                        $returnUser=$nextUser;
+                                            break;
+                                      }
+
+                                      if($reg->interested_in == 'mm'){
+                                          if($nextUser->sex == 'm'){
+                                            $returnUser=$nextUser;
+                                            break;
+                                          }
+                                      }
+
+                                      if($reg->interested_in == 'ff'){
+                                        if($nextUser->sex == 'f'){
+                                          $returnUser=$nextUser;
+                                          break;
+                                        }
+                                      }
+
                                 }
                               }
-
-
-                      }
-
-              }
-              /////
-              if($reg->sex== 'm'){
-                if($nextUser->interested_in == 'mm' or $nextUser->interested_in == 'fm' ){
-
-                            if($reg->interested_in == 'fm'){
-                                  break;
-                            }
-
-                            if($reg->interested_in == 'mm'){
-                                if($nextUser->sex == 'm'){
-                                  break;
-                                }
-                            }
-
-                            if($reg->interested_in == 'ff'){
-                              if($nextUser->sex == 'f'){
-                                break;
-                              }
-                            }
-
-                      }
+                          }
                     }
-
-
-
-                }
-
-
-
-
             }
 
 
-            $dt = Carbon::now();
-            $years = $dt->diffInDays($returnUser->birth_date);
-            $years = floor($years/365);
-            $user = User::find($returnUser->id);
-
-
             $info= array(
-              'number' => $number,
               'iteration' => $iteration,
               'returnUser' => $returnUser,
               'years' => $years,
@@ -126,7 +127,225 @@ class SearchingController extends Controller
 
             );
 
+/********VEROVATNO BI BILO DOBRO DA ISPISUJE I NEKU PORUKU DA TREBA DA SE PROSIRI KRITERIJUM*******/
 
+            if($returnUser == null){
+              return view ('home');
+            }
+
+          return view ('searching', $info);
+      }
+
+
+
+
+      public function likedUser(Request $request)
+      {
+          $user = Auth::user();
+          $reg = Registered_user::find($user->id);
+
+          $iteration = $request->input('iteration');
+          /*
+
+          PROVERA SUPROTNOG MATCH-A, AKO POSTOJI OTVARA SE INTERAKCIJA
+          I BRISE TAJ MATCH
+          AKO NE OVO SE BELEZI KAO MATCH
+
+          */
+
+          //////////////////////////////////
+            $currentIteration = -1;
+            $regUsers = Registered_user::all();
+            $number = $regUsers->count();
+        $returnUser= null;
+      foreach ($regUsers as $nextUser) {
+            $currentIteration ++ ;
+            $returnUser=$nextUser;
+            if($currentIteration > $iteration){
+              $iteration ++ ;
+
+              $dt = Carbon::now();
+              $years = $dt->diffInDays($nextUser->birth_date);
+              $years = floor($years/365);
+
+                      if($nextUser->id != $reg->id){
+
+                        if( $years <= $ageMax and $years >= $ageMin){
+
+                          if($reg->sex== 'f'){
+
+                            if($nextUser->interested_in == 'ff' or $nextUser->interested_in == 'fm' ){
+
+                                          if($reg->interested_in == 'fm'){
+                                                $returnUser=$nextUser;
+                                                break;
+                                          }
+
+                                          if($reg->interested_in == 'mm'){
+                                              if($nextUser->sex == 'm'){
+                                                $returnUser=$nextUser;
+                                                break;
+                                              }
+                                          }
+
+                                          if($reg->interested_in == 'ff'){
+                                            if($nextUser->sex == 'f'){
+                                              $returnUser=$nextUser;
+                                              break;
+                                            }
+                                          }
+
+
+                                  }
+
+                          }
+                          /////
+                          if($reg->sex== 'm'){
+                            if($nextUser->interested_in == 'mm' or $nextUser->interested_in == 'fm' ){
+
+                                        if($reg->interested_in == 'fm'){
+                                            $returnUser=$nextUser;
+                                            break;
+                                        }
+
+                                        if($reg->interested_in == 'mm'){
+                                            if($nextUser->sex == 'm'){
+                                              break;
+                                            }
+                                        }
+
+                                        if($reg->interested_in == 'ff'){
+                                          if($nextUser->sex == 'f'){
+                                            $returnUser=$nextUser;
+                                            break;
+                                          }
+                                        }
+
+                                  }
+                                }
+                            }
+                      }
+
+                }
+
+            }
+
+
+            $info= array(
+              'iteration' => $iteration,
+              'returnUser' => $returnUser,
+              'years' => $years,
+              'user' => $user
+
+
+            );
+
+/********VEROVATNO BI BILO DOBRO DA ISPISUJE I NEKU PORUKU DA TREBA DA SE PROSIRI KRITERIJUM*******/
+            if($returnUser == null){
+              return view ('home');
+            }
+
+
+          return view ('searching', $info);
+      }
+
+/*********ZNAM DA JE OVO UZASNO DUPLIRANJE KODA,
+          ALI AKO PRORADI U NEKOJ KASNIJOJ FAZI CU SREDITI
+          DA BUDE LEPOOOOO***************************************************/
+
+      public function dislikedUser(Request $request)
+      {
+          $user = Auth::user();
+          $reg = Registered_user::find($user->id);
+          $iteration = $request->input('iteration');
+          $currentIteration = -1;
+          $regUsers = Registered_user::all();
+          $number = $regUsers->count();
+          $returnUser= null;
+        foreach ($regUsers as $nextUser) {
+            $currentIteration ++ ;
+            $returnUser=$nextUser;
+            if($currentIteration > $iteration){
+              $iteration ++ ;
+
+              $dt = Carbon::now();
+              $years = $dt->diffInDays($nextUser->birth_date);
+              $years = floor($years/365);
+
+                      if($nextUser->id != $reg->id){
+
+                        if( $years <= $ageMax and $years >= $ageMin){
+
+                          if($reg->sex== 'f'){
+
+                            if($nextUser->interested_in == 'ff' or $nextUser->interested_in == 'fm' ){
+
+                                          if($reg->interested_in == 'fm'){
+                                            $returnUser=$nextUser;
+                                            break;
+                                          }
+
+                                          if($reg->interested_in == 'mm'){
+                                              if($nextUser->sex == 'm'){
+                                                $returnUser=$nextUser;
+                                                break;
+                                              }
+                                          }
+
+                                          if($reg->interested_in == 'ff'){
+                                            if($nextUser->sex == 'f'){
+                                              $returnUser=$nextUser;
+                                              break;
+                                            }
+                                          }
+
+
+                                  }
+
+                          }
+                          /////
+                          if($reg->sex== 'm'){
+                            if($nextUser->interested_in == 'mm' or $nextUser->interested_in == 'fm' ){
+
+                                        if($reg->interested_in == 'fm'){
+                                              break;
+                                        }
+
+                                        if($reg->interested_in == 'mm'){
+                                            if($nextUser->sex == 'm'){
+                                              break;
+                                            }
+                                        }
+
+                                        if($reg->interested_in == 'ff'){
+                                          if($nextUser->sex == 'f'){
+                                            break;
+                                          }
+                                        }
+
+                                  }
+                                }
+                            }
+                      }
+
+                }
+
+            }
+
+
+            $info= array(
+              'iteration' => $iteration,
+              'returnUser' => $returnUser,
+              'years' => $years,
+              'user' => $user
+
+
+            );
+
+/********VEROVATNO BI BILO DOBRO DA ISPISUJE I NEKU PORUKU DA TREBA DA SE PROSIRI KRITERIJUM*******/
+            if($returnUser == null){
+              return view ('home');
+            }
 
 
           return view ('searching', $info);
